2c92f7ee7631f911eca200b10ef5d77e0f98a963
Marcelo Pirk <marcelopirk@marcelos-mbp.home>
2018-08-03 11:32:31 -0300

    implementa plist / headerCell

---
+++ FormFiller/TabelaBrain.swift
@@ -0,0 +1,24 @@
+//
+//  TabelaBrain.swift
+//  FormFiller
+//
+//  Created by Marcelo Pirk on 03/08/18.
+//  Copyright © 2018 Marcelo Pirk. All rights reserved.
+//
+
+import Foundation
+
+extension Tabela {
+    
+    private func makeDados() -> [String:Any] {
+        
+        var retorno:[String:Any] = [:]
+        
+        retorno.updateValue("mainAttr:", forKey: "mainAttr:")
+        
+        
+        return [:]
+        
+    }
+    
+}
---
+++ FormFiller/cells/HeaderCell.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderCell.swift
+//  FormFiller
+//
+//  Created by Marcelo Pirk on 03/08/18.
+//  Copyright © 2018 Marcelo Pirk. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_header: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
